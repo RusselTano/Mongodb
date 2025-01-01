775ba448b8ffbf7c28440d99ae2d38b92c262ef8
Dylane <tanolamene@gmail.com>
2025-01-01 03:18:49 -0400

    Start Chapitre 3 : Introduction aux opérations CRUD

--- testdb.mongodb.js
+++ testdb.mongodb.js
@@ -1,12 +1,20 @@
 use("testdb");
 
 db.users.insertOne({
-  "name": "Alice",
-  "email": "alice@example.com",
-  "password": "citization",
-  "created_at": new Date(),
-  "updated_at": new Date()
 });
 
-db.users.find({
-});
+db.users.find();
+// db.users.insertOne({
+//   name: "didi",
+//   local: {
+//     email: "didi@example.com"
+//   },
+//   address: {
+//     country: "France",
+//     city: "Paris"
+//   }
+// });
+
+// oid ==> object id
+// mogosh mongod compass terminal shell sont des client
+// le driver c'est le langage que vous utilisez pour communiquer avec la base de données ex: nodejs, java, python
