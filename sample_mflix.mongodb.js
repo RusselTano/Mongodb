bae7fca56e8eca116ea6ec98eaa1c7d6696afcb7
Dylane <tanolamene@gmail.com>
2025-01-02 18:12:53 -0400

    Chap6

--- sample_mflix.mongodb.js
+++ sample_mflix.mongodb.js
@@ -1,44 +1,9 @@
-use("school")
+use("sample_supplies")
 
-db.students.findOneAndUpdate({},
-  {
-    $pull: {
-      options: {$gt: 3, $lte: 10},
-    }
-  },
-  {returnDocument: 'after'}
-)
+db.sales.find({},{
+  "customer.gender": 1,
+  "customer.age": 1
+})
 
-
-db.students.findOneAndUpdate({},
-  {
-    $pullAll: {
-      grades: [{score: 12}, {score: 13}],
-    }
-  },
-  {returnDocument: 'after'}
-)
-
-// $ remplace l'indice
-db.students.findOneAndUpdate(
-  {
-    grades: { score: 15 }
-  },
-  {
-    $set: { "grades.$.score" : 12 }
-  },
-  {returnDocument: 'after'}
-)
-
-
-db.students.findOneAndUpdate(
-  {
-    options: { $gte: 12 }
-  },
-  {
-    $inc: { "options.$[]" : 2 }
-  },
-  {returnDocument: 'after'}
-)
-
-//on increment tout le monde si une valeur respecte la condition
+// avec le find
+// find({la condition},{ce que on veut retourner ou affcher},{})
